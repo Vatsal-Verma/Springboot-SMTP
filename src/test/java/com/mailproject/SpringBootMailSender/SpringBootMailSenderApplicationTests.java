c01c20b9235087c27f0fedeafa1f1c785badddab
vatsal_verma <lastav1234@gmail.com>
2025-02-04 00:25:39 +0530

    commit

---
+++ src/test/java/com/mailproject/SpringBootMailSender/SpringBootMailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mailproject.SpringBootMailSender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
