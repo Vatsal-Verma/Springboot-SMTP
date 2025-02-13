ef781b34fbfa427f92e453695c37a88f6e240689
Psychedelics <146416700+Vatsal-Verma@users.noreply.github.com>
2025-02-13 09:24:51 +0530

    Update EmailSenderService.java

--- src/main/java/com/mailproject/SpringBootMailSender/EmailSenderService.java
+++ src/main/java/com/mailproject/SpringBootMailSender/EmailSenderService.java
@@ -13,7 +13,7 @@ public class EmailSenderService {
 
     public void sendEmail(String toEmail, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("lastav1234@gmail.com");
+        message.setFrom("yourname@gmail.com");
         message.setTo(toEmail);
         message.setText(body);
         message.setSubject(subject);
